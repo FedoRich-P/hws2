6a6fe0150646ec509baff537c9da74e9c63898b4
FedoRich-P <panchev_fedor@mail.ru>
2024-09-26 18:01:34 +0300

    h.w.3.3

--- src/s2-homeworks/hw03/GreetingContainer.tsx
+++ src/s2-homeworks/hw03/GreetingContainer.tsx
@@ -18,6 +18,7 @@ export const pureAddUser = (name: string,
     }
     addUserCallback(name)
     setName('')
+    setError('')
 }
 
 export const pureOnBlur = (name: string, setError: (value: string) => void) => { // если имя пустое - показать ошибку
@@ -47,11 +48,10 @@ const GreetingContainer: React.FC<GreetingContainerPropsType> = ({
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
         setName((e.currentTarget.value)) // need to fix
 
-        error && setError('Ошибка! Введите имя!')
+        error && setError('')
     }
     const addUser = () => {
         pureAddUser(name, setError, setName, addUserCallback)
-        console.log(users)
     }
 
     const onBlur = () => {
