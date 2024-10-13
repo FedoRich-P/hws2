3cf85c0a8ba04b8712ad25060fb1c0c939fc9726
FedoRich-P <panchev_fedor@mail.ru>
2024-10-13 20:04:47 +0300

    h.w. 5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,17 +16,27 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path={'/pre-junior'} element={<PreJunior/>}/>
+                <Route path="/" element={<Navigate to={"/pre-junior"}/>}/>
+
+                {/*<Route path={'/junior}'} element={<Junior/>}/>*/}
+                <Route path={'/junior'} element={<Junior/>}/>
+                <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                <Route path="/error" element={<Error404/>}/>
+                <Route path={'/*'} element={<Navigate to={'/error'}/>}/>
+
+
+                {/*<Route path="/pageOne" element={<PageOne/>}/>*/}
+                {/*<Route path="/" element={<Navigate to={"/pageOne"}/>}/>*/}
+                {/*<Route path="/pageTwo" element={<PageTwo/>}/>*/}
+                {/*<Route path="/pageThree" element={<PageThree/>}/>*/}
+                {/*<Route path="/error" element={<Error404/>}/>*/}
+                {/*<Route path="/*" element={<Navigate to={"/error"}/>}/>*/}
+
+
             </Routes>
         </div>
     )
